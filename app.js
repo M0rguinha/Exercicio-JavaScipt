c73048f80e5285085fe3f08e7963af14fc2c2f6a
Morgan <morganrfonseca@gmail.com>
2026-07-01 21:18:35 -0300

    Implement user input prompts and countdown logic
    Added multiple prompts and a countdown loop to the app.

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
+/*
 pontuação = prompt("Qual sua pontuação?");
 if (pontuação >=80 ) {
     alert("Parabéns, você foi aprovado!");
 } else {
     alert("tennte novamente");
 }
+=================
+numero = prompt("informe o numero");
+if (numero > 0){
+    alert('numero positivo')
+} else if (numero < 0){
+    alert('numero nehativo')
+}else{
+    alert('numero 0')
+=================
+let numeroMaximo = prompt('insira um numero pra inicio da contagem regressiva: ');
+let contador = 0;
+
+while (contador <= numeroMaximo) {
+    console.log(contador);
+    contador++;
+}
+=================
+let valor1 = 42;
+let valor2 = 9;
+let resultado = valor1 + valor2;
+
+console.log(´A soma de ${valor1} e ${valor2} é igual a ${resultado}.´);
+*/
